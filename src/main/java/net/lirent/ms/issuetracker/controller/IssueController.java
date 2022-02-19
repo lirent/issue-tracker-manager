2a5277a3c112cf760f856a1bcef458ec81f9e50d
lirent <live@lirent.net>
2022-02-19 22:27:11 +0100

    controller API comments

--- src/main/java/net/lirent/ms/issuetracker/controller/IssueController.java
+++ src/main/java/net/lirent/ms/issuetracker/controller/IssueController.java
@@ -22,12 +22,22 @@ public class IssueController {
     @Autowired
     private IssueServiceImpl issueService;
 
+    /**
+     * Handles the incoming POST API "api/issue/{id}"
+     * @param issue from RequestBody
+     * @return ResponseEntity
+     */
     @PostMapping
     public ResponseEntity<Issue> addIssue(@RequestBody Issue issue){
         return new ResponseEntity<>( issueService.addIssue(issue), HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}") //api/issue/{id}
+    /**
+     * Find issue from a given Id
+     * @param id Issue Id
+     * @return ResponseEntity
+     */
+    @GetMapping("/{id}")
     public ResponseEntity<Issue> findIssue(@PathVariable("id") long id) {
         Optional<Issue> issue = issueService.findIssue(id);
         if (!issue.isPresent()) {
@@ -36,18 +46,32 @@ public class IssueController {
         return new ResponseEntity<>(issue.get(), HttpStatus.OK);
     }
 
+    /**
+     *  Update issue "/api/issue"
+     * @param issue RequestBody
+     * @return ResponseEntity
+     */
     @PutMapping
     public ResponseEntity<Issue> updateIssue(@RequestBody Issue issue){
         return new ResponseEntity<>(issueService.updateIssue(issue), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}") //api/issue/{id}
+    /**
+     * Delete issue from datastore "api/issue/{id}"
+     * @param id Issue Id
+     * @return ResponseEntity
+     */
+    @DeleteMapping("/{id}")
     public ResponseEntity<Issue> deleteIssue(@PathVariable("id") Long id){
         issueService.deleteIssue(id);
         //TODO not-found
         return new ResponseEntity<>( HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Get all issues from DB  "/api/issue"
+     * @return all issues from datastore
+     */
     @GetMapping
     public ResponseEntity<?> findAllIssues(){
         return new ResponseEntity<>(issueService.findAll(), HttpStatus.OK);
