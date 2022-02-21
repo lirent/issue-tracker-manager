29b1685cd9b4b6362fd5fbf1b8430bcd7fc319ee
lirent <live@lirent.net>
2022-02-21 15:19:25 +0100

    issue equals and toString

--- src/main/java/net/lirent/ms/issuetracker/model/Issue.java
+++ src/main/java/net/lirent/ms/issuetracker/model/Issue.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 /**
  * Entity class for Issue tracker
@@ -40,4 +41,30 @@ public class Issue {
     @Version
     private Timestamp timestamp;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Issue issue = (Issue) o;
+        return id.equals(issue.id) && title.equals(issue.title) && description.equals(issue.description) && reporter.equals(issue.reporter) && type == issue.type && priority == issue.priority && status == issue.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, reporter, type, priority, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Issue{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", reporter='" + reporter + '\'' +
+                ", type=" + type +
+                ", priority=" + priority +
+                ", status=" + status +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
