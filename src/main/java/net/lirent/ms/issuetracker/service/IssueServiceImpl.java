ad4349e65fe0ef08045168cdf6661495a746bfa9
lirent <live@lirent.net>
2023-07-19 22:10:22 +0200

    #11 refactor service

--- src/main/java/net/lirent/ms/issuetracker/service/IssueServiceImpl.java
+++ src/main/java/net/lirent/ms/issuetracker/service/IssueServiceImpl.java
@@ -1,10 +1,13 @@
 package net.lirent.ms.issuetracker.service;
 
-import net.lirent.ms.issuetracker.model.Issue;
+import lombok.RequiredArgsConstructor;
+import net.lirent.ms.issuetracker.mapper.IssueMapper;
+import net.lirent.ms.issuetracker.model.dto.IssueRest;
+import net.lirent.ms.issuetracker.model.entity.Issue;
 import net.lirent.ms.issuetracker.repository.IssueRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,33 +18,47 @@ import java.util.Optional;
  */
 
 @Service
+@RequiredArgsConstructor
 public class IssueServiceImpl implements IssueService {
 
-    @Autowired
-    private IssueRepository issueRepository;
+    private final IssueRepository issueRepository;
+    private final IssueMapper issueMapper;
 
     @Override
-    public Issue addIssue(Issue issue) {
-        return issueRepository.save(issue);
+    public Optional<IssueRest> findIssueById(Long issueId) {
+        return issueRepository.findById(issueId).map(issueMapper::entityToIssueRest);
     }
 
     @Override
-    public Optional<Issue> findIssue(Long id) {
-        return issueRepository.findById(id);
+    public IssueRest updateIssue(Long issueId, IssueRest issueRest) {
+        Issue issue = issueRepository.findById(issueId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Issue with id: %s not found".formatted(issueId)
+                ));
+        issue.setDescription(issueRest.getDescription());
+        issue.setPriority(issueRest.getPriority());
+        issue.setStatus(issueRest.getStatus());
+        issue.setTitle(issueRest.getTitle());
+        issue.setReporter(issueRest.getReporter());
+        issue.setType(issueRest.getType());
+        issueRepository.save(issue);
+        return issueRest;
     }
 
     @Override
-    public Issue updateIssue(Issue issue) {
-        return issueRepository.save(issue);
-    }
-
-    @Override
-    public void deleteIssue(Long id) {
+    public void deleteIssueById(Long id) {
         issueRepository.deleteById(id);
     }
 
     @Override
-    public List<Issue> findAll() {
-        return issueRepository.findAll();
+    public List<IssueRest> findAllIssues() {
+        return issueRepository.findAll().stream().map(issueMapper::entityToIssueRest).toList();
+    }
+
+    @Override
+    public IssueRest createNewIssue(IssueRest issueRest) {
+        Issue saved = issueRepository.save(issueMapper.issueRestToEntity(issueRest));
+        issueRest.setIssueId(saved.getIssueId());
+        return issueRest;
     }
 }
